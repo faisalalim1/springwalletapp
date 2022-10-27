3e5b1daefc5b3d532d353bf84d50f61081a62af3
FAISAL ALIM <faisal4all786@gmail.com>
2022-10-27 12:01:30 +0530

    go

---
+++ target/classes/static/precache-manifest.e2143298125ae024480d707a1ef5eeed.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "99b889c730465c0a1964f7aefa871927",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e2747657fd9caeed6336",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "4dbe52f7783308ae4d92",
+    "url": "/static/css/main.00d4352e.chunk.css"
+  },
+  {
+    "revision": "e2747657fd9caeed6336",
+    "url": "/static/js/2.220c3fd0.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.220c3fd0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4dbe52f7783308ae4d92",
+    "url": "/static/js/main.ccdfaa89.chunk.js"
+  },
+  {
+    "revision": "1d039e839164f6abccc7",
+    "url": "/static/js/runtime-main.13b739d4.js"
+  }
+]);
